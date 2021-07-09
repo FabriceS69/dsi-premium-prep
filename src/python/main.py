89473611d1c71f760921cfa4eedeeffbe8a84417
Clark Walker <303walker@gmail.com>
2021-07-08 21:38:08 -0600

    completed day 3

--- src/python/main.py
+++ src/python/main.py
@@ -12,8 +12,9 @@
 
 def main():
     print("\n" * 20) # clear console
-    import day_01_lib
-    import day_02_lib
+    # import day_01_lib
+    # import day_02_lib
+    import day_03_lib
 
 
 if __name__ == "__main__":
