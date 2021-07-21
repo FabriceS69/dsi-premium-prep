9353ed3c922d1b6bd7a5b1b1092b6a24279a13f9
Tovio Roberts <clownfragment@gmail.com>
2021-07-20 20:46:08 -0600

    Auto Updated: Tue 46:20 Jul 20 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -193,13 +193,13 @@ def variance(lst, sample=True):
 
     return total / (len(lst) - sample)
 
-print(list(range(0, 100, 7)))
-print(variance(list(range(0, 100, 7)), sample=False))
-print(variance(list(range(0, 100, 7))))
-print()
-print(list(range(0, 100, 2)))
-print(variance(list(range(0, 100, 2)), sample=False))
-print(variance(list(range(0, 100, 2))))
+# print(list(range(0, 100, 7)))
+# print(variance(list(range(0, 100, 7)), sample=False))
+# print(variance(list(range(0, 100, 7))))
+# print()
+# print(list(range(0, 100, 2)))
+# print(variance(list(range(0, 100, 2)), sample=False))
+# print(variance(list(range(0, 100, 2))))
 
 
 from math import sqrt
@@ -207,10 +207,10 @@ from math import sqrt
 def stdev(lst, sample=True):
     return sqrt(variance(lst, sample))
 
-print(list(range(0, 100, 7)))
-print(stdev(list(range(0, 100, 7)), sample=False))
-print(stdev(list(range(0, 100, 7))))
-print()
-print(list(range(0, 100, 2)))
-print(stdev(list(range(0, 100, 2)), sample=False))
-print(stdev(list(range(0, 100, 2))))
+# print(list(range(0, 100, 7)))
+# print(stdev(list(range(0, 100, 7)), sample=False))
+# print(stdev(list(range(0, 100, 7))))
+# print()
+# print(list(range(0, 100, 2)))
+# print(stdev(list(range(0, 100, 2)), sample=False))
+# print(stdev(list(range(0, 100, 2))))
