d2ae573c70081f89a70141fc7dc16884a01376d1
Tovio Roberts <clownfragment@gmail.com>
2021-07-28 19:23:01 -0600

    Auto Updated: Wed 23:19 Jul 28 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -8,5 +8,7 @@ for pet1 in pets:
             for pet4 in pets:
                 outcomes.append([pet1, pet2, pet3, pet4])
 
-for pet_outcome in outcomes:
-    print(pet_outcome)
+# for pet_outcome in outcomes:
+#     print(pet_outcome)
+
+print(len(outcomes))
