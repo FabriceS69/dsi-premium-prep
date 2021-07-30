fc24c2bac948d422f8c65e3efb60ee02ea3fdea7
Tovio Roberts <clownfragment@gmail.com>
2021-07-29 18:46:20 -0600

    Auto Updated: Thu 46:18 Jul 29 2021

---
+++ src/stats/04_statistical_counting/perms_combs.py
@@ -0,0 +1,23 @@
+
+def factorial(num):
+    prod = 1
+    for n in range(2, num+1):
+        prod *= n
+    return prod
+
+# print(1 / factorial(10))
+
+
+
+
+def permutations(n, k):
+    return int(factorial(n) / factorial(n-k))
+
+def permutations(n, k):
+    perm = 1
+    for i in range(n-k, n+1):
+    # for i in range((n-k)+1, n+1):
+        perm *= i
+    return perm
+
+# print(permutations(10, 4))
